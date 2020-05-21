824836f284a59de5d6d1d19177b24a6b67a7be90
Jenni H <jenni.heikkila1@edu.turkuamk.fi>
2020-05-21 20:57:08 +0300

    Finalized Omnifood
    Animations added only for course reasons

--- Omnifood Example/Resources/JS/script.js
+++ Omnifood Example/Resources/JS/script.js
@@ -60,4 +60,44 @@ $(document).ready(function () {
             }
         });
 
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
+    }, {
+        offset: '50%'
+    });
+
+    /* Mobile navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon ion-icon');
+
+        nav.slideToggle(200);
+        /*if (icon.hasClass('menu-outline')) {
+            icon.addClass('close-outline');
+            icon.removeClass('menu-outline');
+        } else {
+            icon.addClass('menu-outline');
+            icon.removeClass('close-outline');
+        }*/
+        /* This does not work since IonIcons have changed and don't use classes in icons like this. */
+    });
 });
